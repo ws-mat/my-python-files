6bd65f2cb95447240403739a0fb1e86ac04cb0ee
ws-mat <batatanunes037@gmail.com>
2026-04-30 14:31:20 -0400

    sla

--- aula3004.py
+++ aula3004.py
@@ -1,7 +1,10 @@
 #atividade avaliativa dia 30/04, sistema bancário.
-#tem que verificar se o cpf tem dentro do sistema, usando o in
+#tem que verificar se o cpf tem dentro do sistema, usando o in para verificar se o cadastro (todas informações como nome,cpf,senha e etc tem que estar no mesmo index, ex: nome[0],cpf[0])
+
 
 #nao usar o chatgpt, o intuito da aula é ver o aprendizado de cada um
 #dupla sorteada, os 2 em um computador
 #material no teams
 #no final do projeto, a dupla tem que explicar o codigo inteiro, EVITE CHAT GPT!!!!!!!!!!!!!!!!!!
+
+#agr o sistema tem um login e senha para iniciar o msm.
